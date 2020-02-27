f80a59c289f8475beffd00309b79aa9f6db91acd
Kishan Kumar Maurya <kishankumarmaurya@MacBook-Pro.local>
2020-02-27 14:24:11 +0530

    Viewmodel unit testing added

--- app/src/test/java/com/example/githubfirebaseissue/testutil/Rx2SchedulersOverrideRule.kt
+++ app/src/test/java/com/example/githubfirebaseissue/testutil/Rx2SchedulersOverrideRule.kt
@@ -1,21 +1,38 @@
 package com.example.githubfirebaseissue.testutil
 
+import io.reactivex.Scheduler
 import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.disposables.Disposable
+import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.plugins.RxJavaPlugins
-import io.reactivex.schedulers.Schedulers
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
 
 class Rx2SchedulersOverrideRule : TestRule {
+
+    private val immediate = object : Scheduler() {
+        override fun scheduleDirect(run: Runnable, delay: Long, unit: TimeUnit): Disposable {
+            return super.scheduleDirect(run, 0, unit)
+        }
+
+        override fun createWorker(): Worker {
+            return ExecutorScheduler.ExecutorWorker(Executor { it.run() }, false)
+        }
+    }
 
     override fun apply(base: Statement?, description: Description?): Statement =
         object : Statement() {
             override fun evaluate() {
-                RxJavaPlugins.setIoSchedulerHandler { _ -> Schedulers.trampoline() }
-                RxJavaPlugins.setComputationSchedulerHandler { _ -> Schedulers.trampoline() }
-                RxJavaPlugins.setNewThreadSchedulerHandler { _ -> Schedulers.trampoline() }
-                RxAndroidPlugins.setInitMainThreadSchedulerHandler { _ -> Schedulers.trampoline() }
+                RxJavaPlugins.setIoSchedulerHandler { immediate }
+                RxJavaPlugins.setComputationSchedulerHandler {  immediate }
+                RxJavaPlugins.setNewThreadSchedulerHandler { immediate }
+                RxAndroidPlugins.setInitMainThreadSchedulerHandler { immediate }
+                RxJavaPlugins.setInitComputationSchedulerHandler { immediate }
+                RxJavaPlugins.setInitNewThreadSchedulerHandler { immediate }
+                RxJavaPlugins.setInitSingleSchedulerHandler { immediate }
 
                 try {
                     base?.evaluate()
---
+++ app/src/test/java/com/example/githubfirebaseissue/ui/viewmodel/MainViewModelTest.kt
@@ -0,0 +1,138 @@
+package com.example.githubfirebaseissue.ui.viewmodel
+
+import com.example.githubfirebaseissue.model.Issue
+import com.example.githubfirebaseissue.testutil.Rx2SchedulersOverrideRule
+import com.example.githubfirebaseissue.usecase.GetFireBaseIssueUseCase
+import io.reactivex.Single
+import org.junit.Before
+
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.example.githubfirebaseissue.common.Event
+import com.jraska.livedata.TestObserver
+import junit.framework.Assert.assertEquals
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
+
+
+@RunWith(MockitoJUnitRunner::class)
+class MainViewModelTest {
+
+    @JvmField
+    @Rule
+    val rx2SchedulersOverrideRule = Rx2SchedulersOverrideRule()
+
+    @JvmField
+    @Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var fireBaseUseCase: GetFireBaseIssueUseCase
+
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var loadingJraskaTestObserver: TestObserver<Boolean>
+    private lateinit var loadingJraskaTestStates: List<Boolean>
+
+
+    @Mock lateinit var dataObserver: Observer<Event<List<Issue>>>
+    @Mock lateinit var loadingObserver: Observer<Boolean>
+    @Mock lateinit var errorObserver: Observer<Event<Throwable>>
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        viewModel = MainViewModel(fireBaseUseCase)
+    }
+
+
+
+    @Test
+    fun `should return issues when fetching FireBase ios issue using Jraska Library way`() {
+        //Assemble
+        val issueList = listOf<Issue>()
+        `when`(fireBaseUseCase.getFireBaseIosIssues()).thenReturn(Single.just(issueList))
+
+        loadingJraskaTestObserver = TestObserver.test(viewModel.loadingState)
+        loadingJraskaTestStates = loadingJraskaTestObserver.valueHistory()
+
+        val dataObserver = TestObserver.test(viewModel.issueLiveData)
+
+        //Act
+        viewModel.fetchFireBaseIosIssueList()
+
+        //Assert
+        loadingJraskaTestObserver.assertHistorySize(2)
+        assertEquals(true, loadingJraskaTestStates[0])
+        assertEquals(false, loadingJraskaTestStates[1])
+        assertEquals(dataObserver.value().getContentIfNotHandled(),issueList)
+    }
+
+    @Test
+    fun `should return error when fetching FireBase ios issue using Jraska Library way`() {
+        //Assemble
+        val throwable = Throwable("Something went wrong")
+        `when`(fireBaseUseCase.getFireBaseIosIssues()).thenReturn(Single.error(throwable))
+
+        loadingJraskaTestObserver = TestObserver.test(viewModel.loadingState)
+        loadingJraskaTestStates = loadingJraskaTestObserver.valueHistory()
+
+        val errorObserver = TestObserver.test(viewModel.apiError)
+
+        //Act
+        viewModel.fetchFireBaseIosIssueList()
+
+        //Assert
+        loadingJraskaTestObserver.assertHistorySize(3)
+        assertEquals(true, loadingJraskaTestStates[0])
+        assertEquals(false, loadingJraskaTestStates[1])
+        assertEquals(false, loadingJraskaTestStates[2])
+        assertEquals(errorObserver.value().getContentIfNotHandled(),throwable)
+    }
+
+    @Test
+    fun `should return issues when fetching FireBase ios issue using observer way`() {
+        //Assemble
+        val issueList = listOf<Issue>()
+        `when`(fireBaseUseCase.getFireBaseIosIssues()).thenReturn(Single.just(issueList))
+
+        viewModel.issueLiveData.observeForever(dataObserver)
+        viewModel.loadingState.observeForever(loadingObserver)
+        viewModel.apiError.observeForever(errorObserver)
+
+        //Act
+        viewModel.fetchFireBaseIosIssueList()
+
+        //Assert
+        verify(loadingObserver).onChanged(true)
+        verify(loadingObserver).onChanged(false)
+        assertEquals(viewModel.issueLiveData.value?.getContentIfNotHandled(), issueList)
+        assertEquals(viewModel.apiError.value?.getContentIfNotHandled(), null)
+    }
+
+    @Test
+    fun `should return error when fetching FireBase ios issue using observer way`() {
+        //Assemble
+        val throwable = Throwable("Something went wrong")
+        `when`(fireBaseUseCase.getFireBaseIosIssues()).thenReturn(Single.error(throwable))
+
+        viewModel.issueLiveData.observeForever(dataObserver)
+        viewModel.loadingState.observeForever(loadingObserver)
+        viewModel.apiError.observeForever(errorObserver)
+
+        //Act
+        viewModel.fetchFireBaseIosIssueList()
+
+        //Assert
+        verify(loadingObserver).onChanged(true)
+        assertEquals(viewModel.issueLiveData.value?.getContentIfNotHandled(), null)
+        assertEquals(viewModel.apiError.value?.getContentIfNotHandled(), throwable)
+    }
+}
